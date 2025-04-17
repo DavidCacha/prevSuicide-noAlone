f1092635e66b27a74d0b38a36d0611f05599d7ed
LuisDavidCasanovaChavez <luisdcasanova1995@gmail.com>
2025-04-17 02:21:51 -0600

    change of structure of redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,20 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
-import conversationsReducer from '../features/conversations/conversationsSlice';
-import contactReducer from '../features/contact/contactSlice';
-import musicReducer from '../features/music/musicSlice';
-import profileReducer from '../features/profile/profileSlice';
-import locationReducer from '../features/location/locationSlice';
-import asistentReducer from '../features/asistent/asistentSlice';
+import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
-    conversations: conversationsReducer,
-    contacts: contactReducer,
-    music: musicReducer,
-    profile:profileReducer, 
-    location:locationReducer,
-    asistent:asistentReducer,
     user: userReducer
   },
 })
